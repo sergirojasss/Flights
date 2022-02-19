d7307a97b50332f0caba58f735c845b4f1937a55
Sergi Rojas <sergi.rojas@soprasteria.com>
2022-02-19 11:44:46 +0100

    Initial Commit

---
+++ Flights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flights
+//
+//  Created by ROJAS SERRA Sergi on 19/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
